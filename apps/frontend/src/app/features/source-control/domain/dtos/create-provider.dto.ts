22e8e08af4f2afe7b19a773e4de90991039cca9e
Marc Fernandez <mcfdez87@gmail.com>
2026-08-21 17:12:40 +0200

    chore(frontend): remove dead source-control code

--- apps/frontend/src/app/features/source-control/domain/dtos/create-provider.dto.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Data transfer object for registering a new provider
- */
-export interface CreateProviderDto {
-    name: string;
-    appId: string;
-    installationId: string;
-    privateKey: string;
-}
